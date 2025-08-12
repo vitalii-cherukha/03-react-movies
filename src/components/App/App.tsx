7c5793d21bcbff6e6e0de9ea05ffb7ef8ad0455e
vitalii <vitalii.cherukha@gmail.com>
2025-08-12 10:13:15 +0300

    add

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import SearchBar from "../SearchBar/SearchBar"
+import css from './App.module.css'
+
+
+const App = () => {
+  
+
+  
+  return (
+    <div className={css.app}>
+      <SearchBar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,35 @@
+
+import css from './SearchBar.module.css'
+
+
+const SearchBar = () => {
+  return (
+    <header className={css.header}>
+  <div className={css.container}>
+    <a
+      className={css.link}
+      href="https://www.themoviedb.org/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Powered by TMDB
+    </a>
+    <form className={css.form}>
+      <input
+        className={css.input}
+        type="text"
+        name="query"
+        autoComplete="off"
+        placeholder="Search movies..."
+        autoFocus
+      />
+      <button className={css.button} type="submit">
+        Search
+      </button>
+    </form>
+  </div>
+</header>
+  )
+}
+
+export default SearchBar
