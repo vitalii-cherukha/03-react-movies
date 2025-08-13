dfbc3e78fa453470e171b5891a1d072e4e64e28b
vitalii <vitalii.cherukha@gmail.com>
2025-08-13 16:39:25 +0300

    fix-filter

--- src/components/MovieGrid/MovieGrid.tsx
+++ src/components/MovieGrid/MovieGrid.tsx
@@ -10,20 +10,22 @@ const basicsImgUrl: string = 'https://image.tmdb.org/t/p/w500';
 const MovieGrid = ({ onSelect, movies }: MovieGridProps) => {
   return (
     <ul className={css.grid}>
-      {movies.map(movie => (
-        <li key={movie.id}>
-          <div className={css.card}>
-            <img
-              onClick={() => onSelect(movie)}
-              className={css.image}
-              src={basicsImgUrl + movie.poster_path}
-              alt={movie.title}
-              loading="lazy"
-            />
-            <h2 className={css.title}>{movie.title}</h2>
-          </div>
-        </li>
-      ))}
+      {movies
+        .filter(movie => movie.poster_path)
+        .map(movie => (
+          <li key={movie.id}>
+            <div className={css.card}>
+              <img
+                onClick={() => onSelect(movie)}
+                className={css.image}
+                src={basicsImgUrl + movie.poster_path}
+                alt={movie.title}
+                loading="lazy"
+              />
+              <h2 className={css.title}>{movie.title}</h2>
+            </div>
+          </li>
+        ))}
     </ul>
   );
 };
